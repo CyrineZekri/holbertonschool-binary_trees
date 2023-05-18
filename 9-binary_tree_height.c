3adf95911702639b016f3f994dd9763395089438
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-18 17:18:47 +0100

    10th task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -31,7 +31,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 	a = max(binary_tree_height(tree->left), binary_tree_height(tree->right));
 	return (a + 1);
