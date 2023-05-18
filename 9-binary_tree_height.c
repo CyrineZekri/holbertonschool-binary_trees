e3044ca675abd7b73a77cc29a2da63a0d4a68406
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-18 17:17:23 +0100

    10th task

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ *max- finds maximum
+ *@x:an int
+ *@y:an int
+ * Return: signed int
+ */
+int max(int x, int y)
+{
+	if (x > y)
+	{
+		return (x);
+	}
+	else
+	{
+		return (y);
+	}
+
+	/**
+	 * binary_tree_height - Entry point
+	 *@tree:the tree to be measured
+	 * Return: signed int
+	 */
+}
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int a;
+
+	if (tree == NULL)
+	{
+		return (-1);
+	}
+	a = max(binary_tree_height(tree->left), binary_tree_height(tree->right));
+	return (a + 1);
+}
