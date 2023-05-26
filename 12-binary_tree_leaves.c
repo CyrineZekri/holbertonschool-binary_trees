fb1349c0356a8a4049d118eea04776c5a6994842
CyrineZekri <cyrinezekri4@gmail.com>
2023-05-26 23:28:16 +0100

    size of a btree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,11 +1,10 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_leaves- finds if a lead
  *@tree:root node
  * Return: Always 0 (Success)
  */
-size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
 	{
